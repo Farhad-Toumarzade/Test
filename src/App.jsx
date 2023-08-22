16790228dee232a3aff12c9319d0202071f5e912
Farhad-Toumarzade <farhad.thw2@gmail.com>
2023-08-22 11:03:01 +0330

    commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import "./App.css";
+const tabData = [
+  {
+    id: 1,
+    title: "ITEM 1",
+    content: "Authorize the user data ✅",
+  },
+  {
+    id: 2,
+    title: "ITEM 2",
+    content: "Redirect user to cart page 🛒",
+  },
+  {
+    id: 3,
+    title: "ITEM 3",
+    content: "Create new payment for the user 💰",
+  },
+];
+
+function App() {
+  const [activeTab, setActiveTab] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleActiveTab = (id) => {
+    setActiveTab(id);
+  };
+
+  return (
+    <div>
+      <button onClick={() => setIsOpen(!isOpen)}> click here! </button>
+      {isOpen ? (
+        <div className="tab">
+          <div className="tab__header">
+            {tabData.map((tab) => (
+              <button
+                onClick={() => handleActiveTab(tab.id)}
+                key={tab.id}
+                className={activeTab === tab.id ? "active" : ""}
+              >
+                <span>{tab.title}</span>
+                <span className="tab-indicator"></span>
+              </button>
+            ))}
+          </div>
+          <div className="tab__Scontent">{tabData[activeTab - 1].content}</div>
+        </div>
+      ) : (
+        <p>closed!!!!</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
